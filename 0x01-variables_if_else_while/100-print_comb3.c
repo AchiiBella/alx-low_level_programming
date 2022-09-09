93efb367932e2f2eed99e813149bdadee1961ab2
AchiiBella <jeanokello1@gmail.com>
2022-09-09 02:45:49 -0700

    Print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,13 +19,18 @@ int main(void)
 	{
 		j = i + 1;
 		while (j < 58)
-		{																	putchar(i);
+		{
+			putchar(i);
 			putchar(j);
 
-			if (i < 56 || j < 57)														{																	putchar(44);															putchar(32);														}
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 			j++;
 		}
-		
+
 		i++;
 	}
 
