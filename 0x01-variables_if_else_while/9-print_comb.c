8585a23ffd5f86dfa29a61bf488ddb81b39da75d
AchiiBella <jeanokello1@gmail.com>
2022-09-09 02:36:15 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -19,12 +19,12 @@ int main(void)
 		putchar(i);
 		if (i != 57)
 		{
-																			putchar(44);															putchar(32);
-																		}
+			putchar(44);
+			putchar(32);
+		}
 		i++;
 	}
 
 	putchar(10);
-
 	return (0);
 }
