ef51b74a4c659f18fe2ba4a3a8926bf5ac1f2993
AchiiBella <jeanokello1@gmail.com>
2022-10-04 01:07:42 -0700

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ int _strlen(char *s)
 	int size = 0;
 
 	for (; s[size] != '\0'; size++)
-		;
+	;
 	return (size);
 }
 
@@ -35,19 +35,19 @@ if (s1 == NULL)
 if (s2 == NULL)
 	s2 = "\0";
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	m = malloc((size1 + size2) * sizeof(char) + 1);
-	if (m == 0)
-		return (0);
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+m = malloc((size1 + size2) * sizeof(char) + 1);
+if (m == 0)
+	return (0);
 
-	for (i = 0; i <= size1 + size2; i++)
-	{
-		if (i < size1)
-			m[i] = s1[i];
-		else
-			m[i] = s2[i - size1];
+for (i = 0; i <= size1 + size2; i++)
+{
+	if (i < size1)
+		m[i] = s1[i];
+	else
+		m[i] = s2[i - size1];
 	}
-	m[i] = '\0';
-	return (m);
+m[i] = '\0';
+return (m);
 }
